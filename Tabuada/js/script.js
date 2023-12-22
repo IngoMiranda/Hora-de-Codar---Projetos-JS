f53f36faea9d67b8254aeaf4ae785a3809596819
IngoMiranda <ingolima26@gmail.com>
2023-12-22 06:48:32 -0300

    adicionando função ao calc

--- Tabuada/js/script.js
+++ Tabuada/js/script.js
@@ -2,9 +2,36 @@
 
 const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
-const multiplicationInput = document.querySelector("#multiplicator")
-    //funções
-    // eventos
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationOperation = document.querySelector("#multiplication-operation")
+const multiplicationTable = document.querySelector("#multiplication-table");
+const titleSpan = document.querySelector("#multiplication-table span");
+//funções 
+/* Quando a função for executada eu preciso limpar a tabuada anterior ou 
+qualquer tipo de informação no meu containe*/
+
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationOperation.innerHTML = "";
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i
+
+        const tamplate = `<div class="row">
+                <div class="operation">${number} x ${i}</div>
+                <di class="result">${result}</di>
+            </div>`;
+
+
+        const parser = new DOMParser(); //função permite que converta para HTML
+        const tamplateParser = parser.parserFromString(tamplate, "text/html"); //retornando um HTMLDocument ou XMLDocument.
+
+        const row = tamplateParser.querySelector(".row");
+        multiplicationTable.appendChild(row);
+
+        console.log(result);
+    }
+}
+
+// eventos
 
 multiplicationForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,6 +40,8 @@ multiplicationForm.addEventListener("submit", (e) => {
 
     if (!multiplicationNumber || !multiplicatorNumber) return;
 
-    console.log(multiplicationNumber, multiplicatorNumber);
+    createTable(multiplicationNumber, multiplicatorNumber);
+
+    titleSpan.innerHTML = number.value;
 
 });
